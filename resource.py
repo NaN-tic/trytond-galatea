e31857d207288c54afc2f11f6d884ceef4fe8664
Guillem Barba <guillem@nan-tic.com>
2015-02-07 13:46:26 +0100

    Add GalateaVisiblePage class

--- resource.py
+++ resource.py
@@ -2,13 +2,13 @@
 # copyright notices and license terms.
 from trytond.model import ModelSQL, ModelView, fields
 from trytond.pool import Pool
-from trytond.pyson import Bool, If, Eval
+from trytond.pyson import Bool, If, Eval, Greater
 from trytond.transaction import Transaction
 
 from .tools import slugify
 
 __all__ = ['GalateaTemplate', 'GalateaTemplateModel',
-    'GalateaUri']
+    'GalateaUri', 'GalateaVisiblePage']
 
 
 class GalateaTemplate(ModelSQL, ModelView):
@@ -186,6 +186,7 @@ class GalateaUri(ModelSQL, ModelView):
             while slugs:
                 domain.append((prefix + 'slug', clause[1], slugs.pop()))
                 prefix += 'parent.'
+            print "search_uri:", domain
             return domain
         return [
             ('slug',) + tuple(clause[1:]),
@@ -227,3 +228,259 @@ class GalateaUri(ModelSQL, ModelView):
             res = []
         res.append(self)
         return res
+
+
+class GalateaVisiblePage(ModelSQL, ModelView):
+    '''Galatea Visible Page'''
+
+    name = fields.Char('Title', translate=True,
+        required=True, on_change=['name', 'slug'])
+    canonical_uri = fields.Many2One('galatea.uri', 'Canonical URI',
+        required=True, select=True, domain=[
+            # TODO: fail with tag websites functional field
+            # ('website', 'in', Eval('websites')),
+            ('type', '=', 'content'),
+            # This domain clause must to be added in setup() of each subclass
+            # ('template.allowed_models.model', 'in', ['galatea.blog.post']),
+            ],
+        states={
+            'invisible': ~Greater(Eval('id', -1), 0),
+            }, depends=['websites', 'id'])
+    slug = fields.Function(fields.Char('Slug', translate=True, required=True),
+        'on_change_with_slug', setter='set_canonical_uri_field',
+        searcher='search_canonical_uri_field')
+    uris = fields.One2Many('galatea.uri', 'content', 'URIs', readonly=True)
+    uri = fields.Function(fields.Many2One('galatea.uri', 'URI'),
+        'get_uri', searcher='search_uri')
+    uri = fields.Function(fields.Char('Uri'), 'get_uri')
+    # TODO: replace by uri_langs?
+    # slug_langs = fields.Function(fields.Dict(None, 'Slug Langs'),
+    #     'get_slug_langs')
+    # _slug_langs_cache = Cache('galatea_blog_post.slug_langs')
+    # TODO: maybe websites should be a searchable functional field as sum of
+    # canonical_uri/uris website field
+    # websites = fields.Many2Many('galatea.cms.article-galatea.website',
+    #     'article', 'website', 'Websites', required=True,
+    #     help='Tutorial will be available in those websites')
+    visibility = fields.Selection([
+            ('public', 'Public'),
+            ('register', 'Register'),
+            ('manager', 'Manager'),
+            ], 'Visibility', required=True)
+    # TODO: extend getter/setter/searcher to all uris (is active is some uri is
+    # active)
+    active = fields.Function(fields.Boolean('Active',
+            help='Dissable to not show this content.'),
+        'get_active', setter='set_canonical_uri_field',
+        searcher='search_canonical_uri_field')
+    # TODO: move here also the "SEO" fields? (metatitle/description/keywords)
+
+    @classmethod
+    def __setup__(cls):
+        super(GalateaVisiblePage, cls).__setup__()
+
+        uri_domain_clause = ('template.allowed_models.model', 'in',
+            [cls.__name__]),
+        for clause in cls.canonical_uri.domain:
+            if isinstance(clause, tuple) and clause == uri_domain_clause:
+                break
+        else:
+            cls.canonical_uri.domain.append(uri_domain_clause)
+
+    @fields.depends('name', 'slug')
+    def on_change_name(self):
+        res = {}
+        if self.name and not self.slug:
+            res['slug'] = slugify(self.name)
+        return res
+
+    @fields.depends('canonical_uri', 'slug')
+    def on_change_with_slug(self, name=None):
+        if self.canonical_uri:
+            return self.canonical_uri.slug
+        if self.slug:
+            return slugify(self.slug)
+
+    @classmethod
+    def set_canonical_uri_field(cls, records, name, value):
+        pool = Pool()
+        Uri = pool.get('galatea.uri')
+        Uri.write([r.canonical_uri for r in records if r.canonical_uri], {
+                name: value,
+                })
+
+    @classmethod
+    def search_canonical_uri_field(cls, name, clause):
+        domain = [
+            ('canonical_uri.%s' % name,) + tuple(clause[1:]),
+            ]
+        if clause == ['active', '=', False]:
+            domain = [
+                'OR',
+                domain,
+                [('canonical_uri', '=', None)],
+                ]
+        return domain
+
+    # TODO: replace by uri_langs?
+    # def get_slug_langs(self, name):
+    #     'Return dict slugs for each active languages'
+    #     pool = Pool()
+    #     Lang = pool.get('ir.lang')
+    #     Post = pool.get('galatea.blog.post')
+
+    #     post_id = self.id
+    #     langs = Lang.search([
+    #         ('active', '=', True),
+    #         ('translatable', '=', True),
+    #         ])
+
+    #     slugs = {}
+    #     for lang in langs:
+    #         with Transaction().set_context(language=lang.code):
+    #             post, = Post.read([post_id], ['slug'])
+    #             slugs[lang.code] = post['slug']
+
+    #     return slugs
+
+    def get_uri(self, name):
+        context = Transaction().context
+        if context.get('website', False):
+            for uri in self.uris:
+                if uri.website.id == context['website']:
+                    return uri.id
+        return self.canonical_uri.id
+
+    @classmethod
+    def search_uri(cls, name, clause):
+        context = Transaction().context
+        if context.get('website', False):
+            # TODO: is it better and If()?
+            return [
+                ['OR', [
+                    ('canonical_uri',) + tuple(clause[1:]),
+                    ('website', '=', context['website']),
+                    ], [
+                    ('uris',) + tuple(clause[1:]),
+                    ('website', '=', context['website']),
+                    ]],
+                ]
+        return [
+            ['OR', [
+                ('canonical_uri',) + tuple(clause[1:]),
+                ], [
+                ('uris',) + tuple(clause[1:]),
+                ]],
+            ]
+
+    # @classmethod
+    # def default_websites(cls):
+    #     Website = Pool().get('galatea.website')
+    #     websites = Website.search([('active', '=', True)])
+    #     return [w.id for w in websites]
+
+    @staticmethod
+    def default_visibility():
+        return 'public'
+
+    @staticmethod
+    def default_active():
+        return True
+
+    def get_active(self, name):
+        if self.canonical_uri and self.canonical_uri.active:
+            return True
+        return any(u.active for u in self.uris)
+
+    @classmethod
+    def create(cls, vlist):
+        pool = Pool()
+        Uri = pool.get('galatea.uri')
+
+        vlist = [x.copy() for x in vlist]
+        for vals in vlist:
+            if not vals.get('canonical_uri'):
+                assert vals.get('slug')
+                assert vals.get('websites')
+                print 'websites:', vals['websites']
+                uri_vals = cls.calc_uri_vals(vals)
+                print "uri_vals:", uri_vals
+                uri, = Uri.create([uri_vals])
+                vals['canonical_uri'] = uri.id
+        new_records = super(GalateaVisiblePage, cls).create(vlist)
+
+        uri_args = []
+        for record in new_records:
+            if not record.canonical_uri.content:
+                uri_args.extend(([record.canonical_uri], {
+                        'content': str(record),
+                        }))
+        if uri_args:
+            Uri.write(*uri_args)
+        return new_records
+
+    @classmethod
+    def calc_uri_vals(cls, record_vals):
+        website_id = [websites
+            for action, websites in record_vals['websites']
+            if action == 'add'][0][0]
+
+        return {
+            'website': website_id,
+            # 'parent': ,
+            'name': record_vals['name'],
+            'slug': record_vals['slug'],
+            'type': 'content',
+            # 'template': ,
+            'active': record_vals.get('active', cls.default_active()),
+            }
+
+    @classmethod
+    def copy(cls, records, default=None):
+        pool = Pool()
+        Uri = pool.get('galatea.uri')
+
+        if default is None:
+            default = {}
+        else:
+            default = default.copy()
+
+        new_records = []
+        for record in records:
+            default['canonical_uri'], = Uri.copy([record.canonical_uri], {
+                    'slug': '%s-copy' % record.slug,
+                    })
+            new_records += super(GalateaVisiblePage, cls).copy([record],
+                default=default)
+        return new_records
+
+    @classmethod
+    def write(cls, *args):
+        pool = Pool()
+        Uri = pool.get('galatea.uri')
+
+        actions = iter(args)
+        uri_args = []
+        for records, values in zip(actions, actions):
+            if values.get('canonical_uri'):
+                canonical_uri = Uri(values['canonical_uri'])
+                canonical_uri.content = records[0]
+                canonical_uri.save()
+            if 'name' in values:
+                uri_todo = []
+                for record in records:
+                    if record.canonical_uri.name == record.name:
+                        uri_todo.append(record.canonical_uri)
+                    for uri in record.uris:
+                        if uri.name == record.name and uri not in uri_todo:
+                            uri_todo.append(uri)
+                if uri_todo:
+                    # What happens if canonical_uri and name change?
+                    uri_args.append(uri_todo)
+                    uri_args.append({
+                            'name': values['name'],
+                            })
+
+        super(GalateaVisiblePage, cls).write(*args)
+        if uri_args:
+            Uri.write(*uri_args)
