2abe8d516a03e6fbd936963f0446a80643d5894d
Raimon Esteve <raimon@nan-tic.com>
2018-11-27 21:22:30 +0100

    Set none galatea users when copy parties

--- party.py
+++ party.py
@@ -10,3 +10,12 @@ __all__ = ['Party']
 class Party(metaclass=PoolMeta):
     __name__ = 'party.party'
     galatea_users = fields.One2Many('galatea.user', 'party', 'Galatea Users')
+
+    @classmethod
+    def copy(cls, parties, default=None):
+        if default is None:
+            default = {}
+        else:
+            default = default.copy()
+        default.setdefault('galatea_users', None)
+        return super(Party, cls).copy(parties, default=default)
