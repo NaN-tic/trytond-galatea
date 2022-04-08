c3df0b5c9453e6c88588ff82f9a6f9bc0ae53ee0
Raimon Esteve <raimonesteve@gmail.com>
2022-04-08 13:06:55 +0200

    Check rule only if _check_access is set and enforce companies rule issue4080

--- galatea.py
+++ galatea.py
@@ -151,7 +151,10 @@ class GalateaUser(DeactivableMixin, ModelSQL, ModelView, UserMixin):
     _rec_name = 'display_name'
 
     party = fields.Many2One('party.party', 'Party', required=True,
-        ondelete='CASCADE')
+        ondelete='CASCADE', context={
+            'company': Eval('company'),
+            },
+        depends=['company'])
     display_name = fields.Char('Display Name', required=True)
     email = fields.Char("e-Mail", required=True)
     password = fields.Char('Password', required=True)
