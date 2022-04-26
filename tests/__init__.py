d0e0defaed561a1b24416c496ae3d031ca6d23c5
Jared Esparza <jared.esparza@nan-tic.com>
2022-04-26 13:08:44 +0200

    Replace test setuptools command by unittest discover

--- tests/__init__.py
+++
@@ -1,9 +0,0 @@
-# This file is part galatea module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
-try:
-    from trytond.modules.galatea.tests.test_galatea import suite
-except ImportError:
-    from .test_galatea import suite
-
-__all__ = ['suite']
