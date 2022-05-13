4d3920ff15f8a366c8d3a9b1a8aa9b3a3eeb1670
Raimon Esteve <raimonesteve@gmail.com>
2022-05-13 12:13:23 +0200

    Revert "Drop website rule"
    This reverts commit abf7c4179f01d42bab199afda043b458f6dcaf93.

--- tests/test_galatea.py
+++ tests/test_galatea.py
@@ -4,65 +4,13 @@
 # the full copyright notices and license terms.
 import doctest
 import unittest
-from collections import defaultdict
-
 import trytond.tests.test_tryton
-from trytond.model import ModelView, ModelStorage
-from trytond.tests.test_tryton import ModuleTestCase, with_transaction
+from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import doctest_teardown, doctest_checker
 from trytond.modules.company.tests import CompanyTestMixin
-from trytond.pool import Pool, isregisteredby
-from trytond.pyson import Eval, PYSONEncoder
 
 
-class GalateaCompanyTestMixin(CompanyTestMixin):
-
-    # AssertionError Models "sale.shop" are missing a global rule for field "company"
-
-    @with_transaction()
-    def test_company_rule(self):
-        "Test missing company rule"
-        pool = Pool()
-        Rule = pool.get('ir.rule')
-        Company = pool.get('company.company')
-        Employee = pool.get('company.employee')
-        User = pool.get('res.user')
-
-        to_check = defaultdict(set)
-        for mname, model in pool.iterobject():
-            if mname == 'galatea.website':
-                continue
-            if (not isregisteredby(model, self.module)
-                    or model.__access__
-                    or not (issubclass(model, ModelView)
-                        and issubclass(model, ModelStorage))
-                    or issubclass(model, (Company, Employee, User))):
-                continue
-            for fname, field in model._fields.items():
-                if (field._type == 'many2one'
-                        and issubclass(field.get_target(), Company)):
-                    to_check[fname].add(mname)
-
-        for fname, models in to_check.items():
-            rules = Rule.search([
-                    ('rule_group', 'where', [
-                            ('model.model', 'in', list(models)),
-                            ('global_p', '=', True),
-                            ('perm_read', '=', True),
-                            ]),
-                    ('domain', '=', PYSONEncoder(sort_keys=True).encode(
-                            [(fname, 'in', Eval('companies', []))])),
-                    ])
-            with_rules = {r.rule_group.model.model for r in rules}
-            self.assertGreaterEqual(with_rules, models,
-                msg='Models "%(models)s" are missing a global rule '
-                'for field "%(field)s"' % {
-                    'models': ', '.join(models - with_rules),
-                    'field': fname,
-                    })
-
-
-class GalateaTestCase(GalateaCompanyTestMixin, ModuleTestCase):
+class GalateaTestCase(CompanyTestMixin, ModuleTestCase):
     'Test Galatea module'
     module = 'galatea'
 
