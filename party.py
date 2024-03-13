5614e11fd11ed23e68d15f55c3eb8fe253a67124
Raimon Esteve <raimonesteve@gmail.com>
2024-03-13 11:33:08 +0100

    Add phone, mobile, email and fax fields to get/set contact mechanism In case flask has party blueprint, user can add/edit/delete contact mechanisms from address form #065304

--- party.py
+++ party.py
@@ -2,9 +2,8 @@
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 from trytond.model import fields
-from trytond.pool import PoolMeta
-
-__all__ = ['Party', 'PartyReplace']
+from trytond.pool import Pool, PoolMeta
+from trytond.pyson import Eval
 
 
 class Party(metaclass=PoolMeta):
@@ -19,6 +18,59 @@ class Party(metaclass=PoolMeta):
             default = default.copy()
         default.setdefault('galatea_users', None)
         return super(Party, cls).copy(parties, default=default)
+
+
+class Address(metaclass=PoolMeta):
+    __name__ = 'party.address'
+
+    _contact_mechanism_states = {
+        'readonly': Eval('id', -1) >= 0,
+        }
+
+    phone = fields.Function(
+        fields.Char("Phone", states=_contact_mechanism_states),
+        'get_contact_mechanism', setter='set_contact_mechanism')
+    mobile = fields.Function(
+        fields.Char("Mobile", states=_contact_mechanism_states),
+        'get_contact_mechanism', setter='set_contact_mechanism')
+    fax = fields.Function(
+        fields.Char("Fax", states=_contact_mechanism_states),
+        'get_contact_mechanism', setter='set_contact_mechanism')
+    email = fields.Function(
+        fields.Char("E-Mail", states=_contact_mechanism_states),
+        'get_contact_mechanism', setter='set_contact_mechanism')
+
+    def get_contact_mechanism(self, name):
+        for mechanism in self.contact_mechanisms:
+            if mechanism.type == name:
+                return mechanism.value
+        return ''
+
+    @classmethod
+    def set_contact_mechanism(cls, addresses, name, value):
+        pool = Pool()
+        ContactMechanism = pool.get('party.contact_mechanism')
+
+        contact_mechanisms = []
+        to_delete = []
+        for address in addresses:
+            if getattr(address, name):
+                for mechanism in address.contact_mechanisms:
+                    if mechanism.type == name:
+                        break
+                if value:
+                    mechanism.value = value
+                    contact_mechanisms.append(mechanism)
+                else:
+                    to_delete.append(mechanism)
+            elif value:
+                contact_mechanisms.append(ContactMechanism(
+                        party=address.party,
+                        address=address,
+                        type=name,
+                        value=value))
+        ContactMechanism.save(contact_mechanisms)
+        ContactMechanism.delete(to_delete)
 
 
 class PartyReplace(metaclass=PoolMeta):
--- tests/test_module.py
+++ tests/test_module.py
@@ -57,4 +57,35 @@ class GalateaTestCase(CompanyTestMixin, ModuleTestCase):
                     'field': fname,
                     })
 
+    @with_transaction()
+    def test_address_contact_mechanisms(self):
+        'Party Identifiers'
+        pool = Pool()
+        Configuration = pool.get('party.configuration')
+        Party = pool.get('party.party')
+        Address = pool.get('party.address')
+        ContactMechanism = pool.get('party.contact_mechanism')
+
+        party1, = Party.create([{
+                    'name': 'Party 1',
+                    }])
+
+        address, = Address.create([{
+                    'party': party1.id,
+                    'street': 'St sample, 15',
+                    'city': 'City',
+                    }])
+        self.assertTrue(address.id)
+        self.assertEqual(address.email, '')
+        address.email = 'demo@demo.org'
+        address.save()
+        self.assertEqual(address.email, 'demo@demo.org')
+        self.assertEqual(len(address.contact_mechanisms), 1)
+        address.email = 'demo2@demo.org'
+        address.save()
+        self.assertEqual(address.email, 'demo2@demo.org')
+        address.email = None
+        address.save()
+        self.assertEqual(len(address.contact_mechanisms), 0)
+
 del ModuleTestCase
