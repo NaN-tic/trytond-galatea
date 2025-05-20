ea7fea20ab145664c89a234bf17d3fbcd7a2b093
Albert Cervera i Areny <albert@nan-tic.com>
2025-05-20 20:00:35 +0200

    Use trytond's slugify.

--- tools.py
+++ tools.py
@@ -1,31 +1,13 @@
 # This file is part galatea module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-import logging
-
-try:
-    import slug
-except ImportError:
-    logging.getLogger('galatea').error(
-        'Unable to import slug. Install slug package.')
-
-
-def slugify(value):
-    """Convert value to slug: az09 and replace spaces by -"""
-    if not slug:
-        name = ''
-    else:
-        name = slug.slug(value)
-    return name
-
+from trytond.tools import slugify
 
 def slugify_file(value):
     """Convert attachment name to slug: az09 and replace spaces by -"""
-    if not slug:
-        return value
     fname = value.lower().split('.')
     fn = fname[0]
-    name = slug.slug(fn)
+    name = slugify(fn)
 
     if len(fname) > 1:
         return '%s.%s' % (name, fname[1])
